c0ae3fd316668c8321c3efb2c9e2e03df317251c
TranDangHoangAnh <70934104+TranDangHoangAnh@users.noreply.github.com>
2022-03-10 16:20:27 +0700

    Add Adapter Pattern Example

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adapter
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public Employee(int id, string name, string designation, decimal salary)
+        {
+            ID = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+        }
+    }
+    public class ThirdPartyBillingSystem
+    {
+        //ThirdPartyBillingSystem accepts employees information as a List to process each employee salary
+        public void ProcessSalary(List<Employee> listEmployee)
+        {
+            foreach (Employee employee in listEmployee)
+            {
+                Console.WriteLine(employee.Salary + "$ of Salary Credited to " + employee.Name + " Account");
+            }
+        }
+    }
+    public interface ITarget
+    {
+        void ProcessCompanySalary(string[,] employeesArray);
+    }
+    public class EmployeeAdapter : ITarget
+    {
+        ThirdPartyBillingSystem thirdPartyBillingSystem = new ThirdPartyBillingSystem();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            string Id = null;
+            string Name = null;
+            string Designation = null;
+            string Salary = null;
+            List<Employee> listEmployee = new List<Employee>();
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < employeesArray.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        Id = employeesArray[i, j];
+                    }
+                    else if (j == 1)
+                    {
+                        Name = employeesArray[i, j];
+                    }
+                    else if (j == 2)
+                    {
+                        Designation = employeesArray[i, j];
+                    }
+                    else
+                    {
+                        Salary = employeesArray[i, j];
+                    }
+                }
+                listEmployee.Add(new Employee(Convert.ToInt32(Id), Name, Designation, Convert.ToDecimal(Salary)));
+            }
+            Console.WriteLine("Adapter converted Array of Employee to List of Employee");
+            Console.WriteLine("Then delegate to the ThirdPartyBillingSystem for processing the employee salary\n");
+            thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] employeesArray = new string[5, 4]
+            {
+                {"101","John","SE","10000"},
+                {"102","Smith","SE","20000"},
+                {"103","Dev","SSE","30000"},
+                {"104","Pam","SE","40000"},
+                {"105","Sara","SSE","50000"}
+            };
+
+            ITarget target = new EmployeeAdapter();
+            Console.WriteLine("HR system passes employee string array to Adapter\n");
+            target.ProcessCompanySalary(employeesArray);
+            Console.Read();
+        }
+    }
+}
