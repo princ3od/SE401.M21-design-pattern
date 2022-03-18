41f223215283c7663a50bf240dfeebfc5428b558
DucHoang-UIT <jad410.00@gmail.com>
2022-03-18 11:49:13 +0700

    Add example Prototype Pattern

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            Circle circle = new Circle(10,10,20);
+            shapes.Add(circle);
+            Rectangle rectangle = new Rectangle(20,10);
+            shapes.Add(rectangle);
+            List<Shape> copyShapes = new List<Shape>();
+            foreach (var shape in shapes)
+            {
+                copyShapes.Add(shape.Clone());
+            }
+
+            for (int i = 0; i < shapes.Count; i++)
+            {
+                Console.WriteLine($"{shapes[i].GetHashCode()} ------- {copyShapes[i].GetHashCode()}");
+                Console.WriteLine($"I: {shapes[i].ToString()} ------- {copyShapes[i].ToString()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prototype/Prototype/Shape.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Rectangle : Shape
+    {
+        private readonly int _width;
+        private readonly int _height;
+
+        public Rectangle(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+        }
+
+        public Rectangle(Rectangle rectangle) : base(rectangle)
+        {
+            this._width = rectangle._width;
+            this._height = rectangle._height;
+        }
+
+
+        public override Shape Clone()
+        {
+            return new Rectangle(this);
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected int x;
+        protected int y;
+        protected string color;
+        protected Shape(){}
+
+
+        protected Shape(Shape shape)
+        {
+            this.x = shape.x;
+            this.y = shape.y;
+            this.color = shape.color;
+        }
+
+        public abstract Shape Clone();
+
+
+
+        
+
+      
+
+    }
+
+    public class Circle : Shape
+    {
+        private readonly int _radius;
+
+        public Circle(int x, int y, int radius)
+        {
+            this._radius = radius;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Circle(Circle circle) : base(circle)
+        {
+            this._radius = circle._radius;
+        }
+
+
+        public override Shape Clone()
+        {
+            return new Circle(this);
+        }
+    }
+
+}
