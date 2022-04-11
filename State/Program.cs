58d9cabb5cbea60987f219bcb705e06acc116123
DucHoang-UIT <jad410.00@gmail.com>
2022-04-11 14:27:07 +0700

    Add State Pattern Example

---
+++ State/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StatePattern;
+
+namespace RefactoringGuru.DesignPatterns.State.Conceptual
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
+}
