c62ce90036e88084178311206cc9086e3b11c330
TranDangHoangAnh <70934104+TranDangHoangAnh@users.noreply.github.com>
2022-05-12 22:46:38 +0700

    Add Interpreter Pattern

---
+++ Interpreter/Interpreter/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class Context
+    {
+        string input;
+        int output;
+
+        public Context(string input)
+        {
+            this.input = input;
+        }
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+        public int Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+    }
+}
---
+++ Interpreter/Interpreter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string roman = "MCCXXXIV";
+            Context context = new Context(roman);
+
+            // Build the 'parse tree'
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+            // Interpret
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+            Console.WriteLine("{0} = {1}",
+                roman, context.Output);
+       
+            Console.ReadKey();
+        }
+    }       
+}
+
